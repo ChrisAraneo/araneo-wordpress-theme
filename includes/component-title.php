10be925954559996906e1e226bd128c492865255
ChrisAraneo <chris.araneo@gmail.com>
2020-02-23 22:56:43 +0100

    Working on individual components and sections

---
+++ includes/component-title.php
@@ -0,0 +1,5 @@
+<header class="component-title mb-5 mt-5">
+    <h1 class="display-4 text-center font-weight-bold">
+        <?php the_title(); ?>
+    </h1>
+</header>
---
+++ includes/section-post.php
@@ -0,0 +1,19 @@
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+
+<p class="text-center mb-4">
+    <span class="mr-3"><?php the_author_posts_link(); ?></span>
+    <span><?php echo get_the_date('d/m/Y'); ?></span>
+</p>
+
+<?php the_content(); ?>
+
+<?php
+    $tags = get_the_tags();
+    if(!empty($tags)):
+        foreach($tags as &$tag):?> 
+        <a href="<?php echo get_tag_link($tag->term_id);?>">
+            <?php echo $tag->name; ?>
+        </a>
+        <?php endforeach;?>
+    <?php endif;?>
+<?php endwhile; else: endif; ?>
